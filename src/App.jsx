cf6a103182a2575047d609e3fd2d04fd5083f990
mbhatt03 <mihirbhatt03@gmail.com>
2024-06-14 12:00:31 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' bg-slate-900 text-3xl text-white p-4 '>Redux Toolkit</h1>
+    </>
+  )
+}
+
+export default App
